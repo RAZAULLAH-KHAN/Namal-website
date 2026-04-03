2fd1a0b58525331114c3f8d686e2a74e60097396
RAZAULLAH-KHAN <razau@gmail.com>
2026-04-03 16:50:14 +0500

    Configure GitHub Pages with GitHub Actions

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
+  output: 'export',
+  trailingSlash: true,
+  basePath: '/Namal-website',
+  images: {
+    unoptimized: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  }
 };
 
 export default nextConfig;
